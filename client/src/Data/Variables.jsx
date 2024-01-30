2255efb9c3ebbb0246ee6da2dc493ce29f18f40b
rakib8799 <mkrakib007@gmail.com>
2024-01-31 05:38:29 +0600

    deploy on render

--- client/src/Data/Variables.jsx
+++ client/src/Data/Variables.jsx
@@ -1,4 +1,4 @@
 export const serverLink = "https://e-voting-blockchain.onrender.com/api/auth/";
-export const clientLink = "http://localhost:3000/";
+export const clientLink = "https://e-voting-blockchain-client.onrender.com/";
 export const phases = ["init", "voting", "result"];
 export const isFaceRecognitionEnable = true;
